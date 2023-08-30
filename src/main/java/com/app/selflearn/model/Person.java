4275145277a5dbb0410cecef2464ffa4e9157169
Felipe28 <filip.szarek13@gmail.com>
2023-08-30 09:30:26 +0200

    Initialize flyway to project. Create table person. Configure docker-compose. Add @NotNull validation on person variable

--- src/main/java/com/app/selflearn/model/Person.java
+++ src/main/java/com/app/selflearn/model/Person.java
@@ -1,7 +1,11 @@
 package com.app.selflearn.model;
 
 import jakarta.persistence.*;
-import lombok.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -13,11 +17,20 @@ public class Person {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotNull
     private String name;
+
+    @NotNull
     private String surname;
+
+    @NotNull
     private int age;
+
+    @NotNull
     private double height;
 
     @Enumerated(EnumType.STRING)
+    @NotNull
     private Gender gender;
 }
