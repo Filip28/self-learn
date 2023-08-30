72a79dc9ea33650da681966f3de418016b161adb
Felipe28 <filip.szarek13@gmail.com>
2023-08-30 11:44:01 +0200

    Move creation of dummy data from spring repository to flyway migration.

--- src/main/java/com/app/selflearn/SelfLearnApplication.java
+++ src/main/java/com/app/selflearn/SelfLearnApplication.java
@@ -1,35 +1,13 @@
 package com.app.selflearn;
 
-import com.app.selflearn.database.PersonRepository;
-import com.app.selflearn.model.Gender;
-import com.app.selflearn.model.Person;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SelfLearnApplication implements CommandLineRunner {
+public class SelfLearnApplication {
 
-	private final PersonRepository personRepository;
+    public static void main(String[] args) {
+        SpringApplication.run(SelfLearnApplication.class, args);
+    }
 
-	public SelfLearnApplication(PersonRepository personRepository) {
-		this.personRepository = personRepository;
-	}
-
-	public static void main(String[] args) {
-		SpringApplication.run(SelfLearnApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) {
-//		List<Person> persons = List.of(
-//				new Person(1,"John", "Doe", 19, 1.85, Gender.MAN),
-//				new Person(2,"Adrianna", "Dickie", 39, 1.45, Gender.WOMAN),
-//				new Person(3,"Flower", "Noone", 24, 1.74, Gender.OTHER)
-//		);
-//		personRepository.saveAll(persons);
-		personRepository.save(new Person(1L,"John", "Doe", 19, 1.85, Gender.MAN));
-		personRepository.save(new Person(2L,"Adrianna", "Dickie", 39, 1.45, Gender.WOMAN));
-		personRepository.save(new Person(3L,"Flower", "Noone", 24, 1.74, Gender.OTHER));
-	}
 }
---
+++ src/main/resources/db/migration/V2__insert_person_data.sql
@@ -0,0 +1,4 @@
+insert into person (name, surname, age, height, gender)
+values ('John', 'Doe', 19, 1.85, 'MAN'),
+       ('Adrianna', 'Dickie', 39, 1.45, 'WOMAN'),
+       ('Flower', 'Noone', 24, 1.74, 'OTHER')
