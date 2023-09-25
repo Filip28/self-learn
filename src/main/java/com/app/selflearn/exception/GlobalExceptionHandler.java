3e3525e13b2af902ef16b5029e78402f22da8ee0
Felipe28 <filip.szarek13@gmail.com>
2023-09-25 18:58:58 +0200

    Change creation of error response info. Now we will show which filed have got errors and what are that these errors.

--- src/main/java/com/app/selflearn/exception/GlobalExceptionHandler.java
+++ src/main/java/com/app/selflearn/exception/GlobalExceptionHandler.java
@@ -1,18 +1,18 @@
 package com.app.selflearn.exception;
 
-import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
@@ -20,13 +20,17 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
         Map<String, Object> body = new HashMap<>();
 
-        List<String> errors = ex.getBindingResult()
-                .getFieldErrors()
-                .stream()
-                .map(DefaultMessageSourceResolvable::getDefaultMessage)
-                .toList();
+        ex.getBindingResult().getAllErrors().forEach(error -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            if (body.containsKey(fieldName)) {
+                Object value = body.get(fieldName);
+                body.put(fieldName, Objects.requireNonNull(errorMessage).concat(value.toString()));
+            } else {
+                body.put(fieldName, errorMessage);
+            }
+        });
 
-        body.put("errors", errors);
         body.put("statusCode", status.value());
 
         return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
